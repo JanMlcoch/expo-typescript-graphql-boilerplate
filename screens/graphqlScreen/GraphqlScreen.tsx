01c3abcd209541b00df61b608502176d5b1b9c0c
Jan Mlčoch <mlcoch.jan@gmail.com>
2019-03-05 17:24:42 +0100

    Add example for same variables query

--- screens/graphqlScreen/GraphqlScreen.tsx
+++ screens/graphqlScreen/GraphqlScreen.tsx
@@ -1,24 +1,42 @@
 import React, {Component} from 'react';
-import {ScrollView, Text} from 'react-native';
+import {ScrollView, Text, TouchableHighlight} from 'react-native';
 import {Query, QueryResult} from 'react-apollo';
 import {styles} from './styles';
 import getPokemonMaxHPGraphql from 'getPokemonMaxHP.graphql';
 
-export default class GraphqlScreen extends Component {
+interface GraphqlScreenState {
+  randomNumber: number;
+}
+
+export default class GraphqlScreen extends Component<{}, GraphqlScreenState> {
   static navigationOptions = {
-    title: 'Links',
+    title: 'Graphql',
+  };
+
+  state = {
+    randomNumber: Math.random(),
+  };
+
+  changeNumber = () => {
+    this.setState({
+      randomNumber: Math.random(),
+    })
   };
 
   render() {
     return (
       <ScrollView style={styles.container}>
         {this.renderQuery()}
+        <Text>Random number: {this.state.randomNumber}</Text>
+        <TouchableHighlight style={styles.button} onPress={this.changeNumber}>
+          <Text>Change random number</Text>
+        </TouchableHighlight>
       </ScrollView>
     );
   }
 
   renderQuery = () => {
-    console.log('renderig query');
+    console.log('Rendering Pikachu query');
     const variables = {
       name: 'pikachu',
     };
@@ -30,6 +48,8 @@ export default class GraphqlScreen extends Component {
   };
 
   renderQueryResult = ({data, loading}: QueryResult) => {
+    // HERE: I got cached data here! You can see there is no request in reactotron
+    console.log('New result', data && data.pokemon);
     if (loading) {
       return <Text>Pikachu maxHP: loading</Text>;
     }
--- screens/graphqlScreen/styles.ts
+++ screens/graphqlScreen/styles.ts
@@ -6,4 +6,13 @@ export const styles = StyleSheet.create({
     paddingTop: 15,
     backgroundColor: '#fff',
   },
+  button: {
+    height: 50,
+    width: 200,
+    borderWidth: 1,
+    borderColor: 'black',
+    alignContent: 'center',
+    margin: 10,
+    padding: 10,
+  },
 });
